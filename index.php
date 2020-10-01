b42a8b64803023db2ff8e394d584d21cbfdcebae
tiger-monster <github.com>
2020-10-01 14:25:32 -0500

    Nueva version 2

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    <script src="Librearias/Data Tables/jquery.dataTables.min.js"></script>
-    <script src="Librearias/Data Tables/dataTables.bootstrap4.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="Librearias/Data Tables/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="Librearias/Data Tables/dataTables.bootstrap4.min.css">
+    <script src="Librerias/Data Tables/jquery.dataTables.min.js"></script>
+    <script src="Librerias/Data Tables/dataTables.bootstrap4.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="Librerias/Data Tables/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="Librerias/Data Tables/dataTables.bootstrap4.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link href="Librearias/fontawesome-free-5.14.0-web/css/all.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css">
-<link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/bootstrap.css">
-<script src="librerias/alertify/alertify.js"></script>
+  <link href="Librerias/fontawesome-free-5.14.0-web/css/all.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="Librerias/alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="Librerias/alertify/css/themes/bootstrap.css">
+<script src="Librerias/alertify/alertify.js"></script>
 </head>
 <body style="background-color:#20B2AA;">
   <div class="container">
--- scripts.php
+++ scripts.php
@@ -2,8 +2,8 @@
 <meta charset="utf-8">
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-<script src="Librearias/Data Tables/jquery.dataTables.min.js"></script>
-<script src="Librearias/Data Tables/dataTables.bootstrap4.min.js"></script>
+<script src="Librerias/Data Tables/jquery.dataTables.min.js"></script>
+<script src="Librerias/Data Tables/dataTables.bootstrap4.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="Librerias/jquery.min.js"></script>
@@ -11,10 +11,10 @@
 <script src="Librerias/bootstrap/bootstrap.min.js"></script>
 <script src="Librerias/alertify/alertify.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="librerias/bootstrap/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="librerias/Data Tables/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="librerias/Data Tables/dataTables.bootstrap4.min.css">
-<link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css">
-<link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="Librearias/fontawesome-free-5.14.0-web/css/all.css">
+<link rel="stylesheet" type="text/css" href="Librerias/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="Librerias/Data Tables/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="Librerias/Data Tables/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="Librerias/alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="Librerias/alertify/css/themes/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="Librerias/fontawesome-free-5.14.0-web/css/all.css">
 
